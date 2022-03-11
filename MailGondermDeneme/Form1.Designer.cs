d1db20db9c457801e9d05d061ff2118b0b048ef6
Betül Ölçer <betulkok.0@gmail.com>
2022-03-11 14:32:24 +0300

    Add project files.

---
+++ MailGondermDeneme/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace MailGondermDeneme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.Gonder = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(24, 30);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(173, 137);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Gonder
+            // 
+            this.Gonder.Location = new System.Drawing.Point(24, 205);
+            this.Gonder.Name = "Gonder";
+            this.Gonder.Size = new System.Drawing.Size(173, 23);
+            this.Gonder.TabIndex = 1;
+            this.Gonder.Text = "Gonder";
+            this.Gonder.UseVisualStyleBackColor = true;
+            this.Gonder.Click += new System.EventHandler(this.Gonder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(238, 365);
+            this.Controls.Add(this.Gonder);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button Gonder;
+    }
+}
+
---
+++ MailGondermDeneme/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net.Mail;
+using System.IO;
+
+namespace MailGondermDeneme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Gonder_Click(object sender, EventArgs e)
+        {
+            GonderMail("deneme yapıyoruz.", richTextBox1.Text);
+        }
+        
+
+        private bool GonderMail(string konu, string icerik)
+        {
+            SmtpClient smtp = new SmtpClient();
+            smtp.Credentials = new System.Net.NetworkCredential("keepfitTeam1@outlook.com", sifreoku());
+            smtp.Port = 587;
+            smtp.Host = "smtp-mail.outlook.com";
+            smtp.EnableSsl = true;
+
+            MailMessage ePosta = new MailMessage();
+            ePosta.To.Add("keepfitTeam1@outlook.com");
+            ePosta.From = new MailAddress("keepfitTeam1@outlook.com");
+            ePosta.IsBodyHtml = true;
+            ePosta.Subject = konu;
+            ePosta.Body = $"<h1>{icerik}</h1>" ;
+
+            object userState = ePosta;
+            bool kontrol = true;
+
+            try
+            {
+                smtp.SendAsync(ePosta, (object)ePosta);
+                MessageBox.Show("Mail gondemr başarılı");
+
+            }
+            catch (SmtpException ex)
+            {
+
+                kontrol = false;
+                System.Windows.Forms.MessageBox.Show(ex.Message , "Başarısız oldu.");
+            }
+
+            return kontrol;
+            
+        }
+
+        private string sifreoku()
+        {
+            return File.ReadAllText(@"C:\Users\betul\source\repos\MailGondermDeneme\sifre.txt");
+
+        }
+    }
+}
